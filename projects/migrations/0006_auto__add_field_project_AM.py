316fa2d6dedc956c139b5b726769664120b29506
wpe-onboarding <matthew@wpengine.com>
2014-07-14 16:39:18 -0500

    work done at work

---
+++ projects/migrations/0006_auto__add_field_project_AM.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Project.AM'
+        db.add_column(u'projects_project', 'AM',
+                      self.gf('django.db.models.fields.CharField')(max_length=30, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Project.AM'
+        db.delete_column(u'projects_project', 'AM')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'projects.category': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Category'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '30'})
+        },
+        u'projects.comment': {
+            'Meta': {'object_name': 'Comment'},
+            'created': ('django.db.models.fields.DateField', [], {}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'link': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.Link']"}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.Project']"}),
+            'risk': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.Risk']"}),
+            'task': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.Task']"})
+        },
+        u'projects.link': {
+            'Meta': {'object_name': 'Link'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'created': ('django.db.models.fields.DateField', [], {}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.Project']"})
+        },
+        u'projects.project': {
+            'AM': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'}),
+            'Meta': {'ordering': "['acctName']", 'object_name': 'Project'},
+            'PM': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            'acctName': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.Category']"}),
+            'completedDate': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateField', [], {}),
+            'endDate': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'isArchived': ('django.db.models.fields.BooleanField', [], {}),
+            'isCompleted': ('django.db.models.fields.BooleanField', [], {}),
+            'isDeleted': ('django.db.models.fields.BooleanField', [], {}),
+            'startDate': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'projects.risk': {
+            'Meta': {'object_name': 'Risk'},
+            'created': ('django.db.models.fields.DateField', [], {}),
+            'doesImpactCost': ('django.db.models.fields.BooleanField', [], {}),
+            'doesImpactPerformance': ('django.db.models.fields.BooleanField', [], {}),
+            'doesImpactSchedule': ('django.db.models.fields.BooleanField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'impact': ('django.db.models.fields.IntegerField', [], {'default': '5'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'plan': ('django.db.models.fields.TextField', [], {}),
+            'probability': ('django.db.models.fields.IntegerField', [], {'default': '5'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.Project']"})
+        },
+        u'projects.task': {
+            'Meta': {'object_name': 'Task'},
+            'PM': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            'completedDate': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateField', [], {}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'endDate': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'isCompleted': ('django.db.models.fields.BooleanField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'startDate': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'tasklist': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.TaskList']"})
+        },
+        u'projects.tasklist': {
+            'Meta': {'object_name': 'TaskList'},
+            'created': ('django.db.models.fields.DateField', [], {}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'isTemplate': ('django.db.models.fields.BooleanField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.Project']", 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['projects']
